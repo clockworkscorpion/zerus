8378bd6303f4c7bf1cc372861d2b8c23ed4d6de4
clockworkscorpion <faheem.shereef@outlook.com>
2023-03-16 11:25:39 +0400

    Padding change

--- styles/index.js
+++ styles/index.js
@@ -1,5 +1,5 @@
 const styles = {
-  innerWidth: "2xl:max-w-[720px] w-full",
+  innerWidth: "2xl:max-w-[1280px] w-full",
   interWidth: "lg:w-[100%] w-[100%]",
 
   paddings: "sm:p-16 xs:p-8 px-6 py-12",
